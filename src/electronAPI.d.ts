66d2d274238fc051b9a329be685deeb103f78caf
moonhr <moonhr153@gmail.com>
2024-09-20 19:40:35 +0900

    Feat | electronAPI.d.ts
    ts에게 electronAPI가 존재한다는 것을 알리기 위함 renderer 프로세스에서 electronAPI를 찾지 못하는 에러를 해결함

---
+++ src/electronAPI.d.ts
@@ -0,0 +1,12 @@
+// ts에게 electronAPI가 존재한다는 것을 알리기 위함
+// renderer 프로세스에서 electronAPI를 찾지 못하는 에러를 해결함
+export {};
+
+declare global {
+  interface Window {
+    electronAPI: {
+      sendPing: (message: string) => void;
+      onPong: (callback: (message: string) => void) => void;
+    };
+  }
+}
