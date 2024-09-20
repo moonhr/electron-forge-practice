eedc755bc7a6b6357b535d61d916ba6c121b9f32
moonhr <moonhr153@gmail.com>
2024-09-20 19:31:15 +0900

    typescript 설치

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,5 +1,5 @@
-const input = document.getElementById("input");
-const sendButton = document.getElementById("sendButton");
+const input = document.getElementById("input")!;
+const sendButton = document.getElementById("sendButton")!;
 
 // 버튼을 클릭했을 때 ping 이벤트 보내기
 sendButton.addEventListener("click", () => {
