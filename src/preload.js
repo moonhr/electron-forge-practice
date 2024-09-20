7bb43d70675cf99526de169b524c8d44e5487505
moonhr <moonhr153@gmail.com>
2024-09-20 19:26:59 +0900

    ipcRenderer를 정의하여 보안을 강화하며 렌더러에서 node.js에 바로 접근하는것을 막음

--- src/preload.js
+++ src/preload.js
@@ -1,2 +1,7 @@
-// See the Electron documentation for details on how to use preload scripts:
-// https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+const { contextBridge, ipcRenderer } = require("electron");
+
+contextBridge.exposeInMainWorld("electronAPI", {
+  sendPing: (message) => ipcRenderer.send("ping", message), // ping 이벤트를 메인 프로세스로 보냄
+  onPong: (callback) =>
+    ipcRenderer.on("pong", (event, message) => callback(message)), // 메인 프로세스로부터 pong 이벤트 수신
+});
