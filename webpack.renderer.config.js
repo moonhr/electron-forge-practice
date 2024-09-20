d6d5f196391cda329509c37306b48b6ed3d322aa
moonhr <moonhr153@gmail.com>
2024-09-20 19:35:14 +0900

    Feat | 렌더러 프로세스용 웹팩 설정

---
+++ webpack.renderer.config.js
@@ -0,0 +1,22 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/renderer.ts",
+  target: "electron-renderer",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        include: /src/,
+        use: "ts-loader",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  output: {
+    filename: "renderer.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+};
